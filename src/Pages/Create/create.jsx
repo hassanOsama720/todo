2a942b3b97885f096083f87c51033bbbff126490
Hassan_Osama <hassan.osama720@gmail.com>
2023-05-22 21:15:27 +0300

    update json server link

--- src/Pages/Create/create.jsx
+++ src/Pages/Create/create.jsx
@@ -24,7 +24,7 @@ const Create = () => {
                 navigate("/");
             })
             .catch((err) => {
-                console.log(err);
+                console.log(err.response);
             })
     }
 
